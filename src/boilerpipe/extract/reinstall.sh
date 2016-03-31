f83d9a547705b83d4538cd9cf6b02ec8b6a43e86
saschwafel <saschwafel@gmail.com>
2016-03-31 13:56:02 -0700

    Adding breakpoint

---
+++ src/boilerpipe/extract/reinstall.sh
@@ -0,0 +1,10 @@
+ 6093  git add .
+ 6094  git commit -m "Trying another fix for Unicode errors"
+ 6095  git add .
+ 6096  git commit -m "Trying another fix for Unicode errors"
+ 6097  git push https://github.com/saschwafel/python-boilerpipe-customized.git
+ 6098  sudo pip install --upgrade git+https://github.com/saschwafel/python-boilerpipe-customized.git
+ 6099* python node_client.py 
+ 6100  touch reinstall.sh
+ 6101  vim reinstall.sh 
+ 6102  history|tail -10
